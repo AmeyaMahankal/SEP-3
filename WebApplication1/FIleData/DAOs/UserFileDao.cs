5b47fd27c9495e63ad7f981f300a7e3dffceb721
ldomljan <118074075+ldomljan@users.noreply.github.com>
2023-03-19 15:53:38 +0100

    FirstFeature
    Co-Authored-By: AmeyaMahankal <104625478+AmeyaMahankal@users.noreply.github.com>
    Co-Authored-By: joan1821 <104628539+joan1821@users.noreply.github.com>
    Co-Authored-By: Zsolt698 <104628450+Zsolt698@users.noreply.github.com>

---
+++ WebApplication1/FIleData/DAOs/UserFileDao.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata;
+using Application.DaoInterfaces;
+using Domain.Models;
+
+namespace FileData.DAOs;
+
+public class UserFileDao: IUserDao
+{
+    private readonly FileContext context;
+
+    public UserFileDao(FileContext context)
+    {
+        this.context = context;
+    }
+
+    public Task<User> CreateAsync(User user)
+    {
+        int Id = 1;
+        if (context.Users.Any())
+        {
+            Id = context.Users.Max(u => u.Id);
+            Id++;
+        }
+
+        user.Id = Id;
+        context.Users.Add(user);
+        context.SaveChanges();
+        return Task.FromResult(user);
+    }
+
+    public Task<User?> GetByUsernameAsync(string userName)
+    {
+        User? existing =
+            context.Users.FirstOrDefault(u => u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase));
+        return Task.FromResult(existing);
+    }
+    
+}
