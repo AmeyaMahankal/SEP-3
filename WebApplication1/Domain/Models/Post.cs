2164497ffd546c9a4a7816bf9026e3d31a7e0589
AmeyaMahankal <ameya.mahankal@gmail.com>
2023-03-19 13:14:46 +0100

    BasicSetupComplete
    Co-Authored-By: joan1821 <104628539+joan1821@users.noreply.github.com>
    Co-Authored-By: ldomljan <118074075+ldomljan@users.noreply.github.com>

---
+++ WebApplication1/Domain/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+
+    public User Poster { get; }
+    
+    public string PostTitle { get; set; }
+    
+    public string PostBody { get; set; }
+
+    public Post(User poster, string postTitle, string postBody)
+    {
+        Poster = poster;
+        PostTitle = postTitle;
+        PostBody = postBody;
+    }
+}
