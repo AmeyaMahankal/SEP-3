e1e6b429ec82b036cb500eea88e1e9e6f6b6bfd3
AmeyaMahankal <ameya.mahankal@gmail.com>
2023-03-26 18:24:00 +0200

    Viewpasswords(ViewUsers)
    Username and id doesnt appear

--- WebApplication1/HttpClients/Implementations/UserHttpClient.cs
+++ WebApplication1/HttpClients/Implementations/UserHttpClient.cs
@@ -28,4 +28,23 @@ public class UserHttpClient: IUserService
         User user = JsonSerializer.Deserialize<User>(result)!;
         return user;
     }
+
+    public async Task<IEnumerable<User>> GetUsers(string? usernameContains = null)
+    {
+        string uri = "/user";
+        if (!string.IsNullOrEmpty(usernameContains))
+        {
+            uri += $"?username={usernameContains}";
+        }
+
+        HttpResponseMessage response = await client.GetAsync(uri);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        IEnumerable<User> users = JsonSerializer.Deserialize<IEnumerable<User>>(result);
+        return users;
+    }
 }
