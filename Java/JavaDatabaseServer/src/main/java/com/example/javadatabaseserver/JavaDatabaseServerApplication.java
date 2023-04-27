09899ee6ff01b3865adcd89675029f4643945344
AmeyaMahankal <ameya.mahankal@gmail.com>
2023-04-27 09:56:46 +0200

    JavaProjectAdded

---
+++ Java/JavaDatabaseServer/src/main/java/com/example/javadatabaseserver/JavaDatabaseServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.javadatabaseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDatabaseServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDatabaseServerApplication.class, args);
+	}
+
+}
