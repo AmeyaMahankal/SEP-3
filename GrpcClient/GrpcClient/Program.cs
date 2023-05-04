a2a025aec71b078bc264bd99f9d9d9f38efe0467
joan1821 <gwantamo35@gmail.com>
2023-05-04 10:52:02 +0200

    Grpc client fOR C#

---
+++ GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Grpc.Net.Client;
+
+class Programs
+{
+    static async Task Main(string[] args)
+    {
+        var inPut = new UserGetUsername();
+        var channel = GrpcChannel.ForAddress("http://localhost:9090");
+        var client = new Access.AccessClient(channel);
+        UserGetUsername request = new UserGetUsername
+        {
+            Username = "jeff"
+        };
+        User reply = await client.GetByUsernameAsync(request);
+        Console.WriteLine(reply.Password);
+
+
+    }
+    
+    
+}
