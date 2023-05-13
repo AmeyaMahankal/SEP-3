a3ad6cc71206fd25d5905a389509ca8a81fc62b3
ldomljan <118074075+ldomljan@users.noreply.github.com>
2023-05-13 14:08:06 +0200

    rest,park,mus,daoADDED

---
+++ WebApplication1/FIleData/GrpcDAOs/ParkDAO.cs
@@ -0,0 +1,57 @@
+﻿using Application.DaoInterfaces;
+using DataAccessClient;
+using Grpc.Net.Client;
+using sep3.DTOs;
+using Park = SEP3lu.Park;
+
+namespace FileData.GrpcDAOs;
+
+public class ParkDAO : IParkDao
+{
+    private Access.AccessClient client;
+
+    public ParkDAO()
+    {
+        var channel = GrpcChannel.ForAddress("http://localhost:9090");
+        client = new Access.AccessClient(channel);
+
+    }
+
+    public Task<Park> CreateAsync(Park park)
+    {
+        ParkToCreate request = new ParkToCreate()
+        {
+            Name = park.Name,
+            Description = park.Description,
+            Imageurl = park.ImageURL,
+            Cityid = park.CityId
+        };
+        var send = client.CreatePark(request);
+
+        return Task.FromResult(park);
+    }
+
+    public Task<IEnumerable<Park>> GetAsync(SearchParkParametersDto searchParameters)
+    {
+        throw new NotImplementedException();
+       }
+   
+   
+   public Task<Park?> GetByIdAsync(int id)
+       {
+           throw new NotImplementedException(); 
+       }
+
+   public Task UpdateAsync(Park updated)
+   {
+       throw new NotImplementedException();
+   }
+
+   public Task DeleteAsync(int id)
+       {
+           throw new NotImplementedException();
+       }
+      
+    
+}
+
---
+++ WebApplication1/FIleData/GrpcDAOs/RestaurantDAO.cs
@@ -0,0 +1,54 @@
+﻿using Application.DaoInterfaces;
+using DataAccessClient;
+using Grpc.Net.Client;
+using sep3.DTOs;
+using Restaurant = SEP3lu.Restaurant;
+
+namespace FileData.GrpcDAOs;
+
+public class RestaurantDAO : IRestaurantDao
+{
+    
+    private Access.AccessClient client;
+    
+    public RestaurantDAO()
+    {
+        var channel = GrpcChannel.ForAddress("http://localhost:9090");
+        client = new Access.AccessClient(channel);
+    }
+    
+    
+    public Task<Restaurant> CreateAsync(Restaurant restaurant)
+    {
+        RestaurantToCreate request = new RestaurantToCreate()
+        {
+            Name = restaurant.Name,
+            Description = restaurant.Description,
+            Imageurl = restaurant.ImageURL,
+            Cityid = restaurant.CityId
+        };
+        var send = client.CreateRestaurant(request);
+        
+        return Task.FromResult(restaurant);
+    }
+
+    public Task<IEnumerable<Restaurant>> GetAsync(SearchRestaurantParametersDto searchRestaurantParameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Restaurant?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Restaurant updated)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
