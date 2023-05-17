fa43e774dc945aae2cb7a65cf99d59f7036e2d54
AmeyaMahankal <ameya.mahankal@gmail.com>
2023-05-17 17:42:35 +0200

    BASECreated

--- WebApplication1/HttpClients/ClientInterfaces/IRestaurantService.cs
+++ WebApplication1/HttpClients/ClientInterfaces/IRestaurantService.cs
@@ -1,8 +1,11 @@
-﻿using SEP3lu;
+﻿using sep3.DTOs;
+using SEP3lu;
 
 namespace HttpClients.ClientInterfaces;
 
 public interface IRestaurantService
 {
     public Task<IEnumerable<Restaurant>?> getRestaurants(int id);
+
+    Task<Restaurant> CreateRestaurant(RestaurantCreationDto dto);
 }
