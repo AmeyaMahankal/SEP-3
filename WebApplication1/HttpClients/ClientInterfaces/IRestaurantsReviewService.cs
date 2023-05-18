ecf52b1a69c2d5ba78b01c4278a7297eadc20c5d
joan1821 <gwantamo35@gmail.com>
2023-05-18 20:58:32 +0200

    added the Review for the restaurant
    added the skeleton for the restaurants review

---
+++ WebApplication1/HttpClients/ClientInterfaces/IRestaurantsReviewService.cs
@@ -0,0 +1,13 @@
+﻿using Domain.DTOs;
+using Domain.Models;
+using sep3.DTOs;
+using SEP3lu;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IRestaurantsReviewService
+{
+    public Task<IEnumerable<Review>?> getRestaurantsReviews(int restaurantId);
+
+    Task<Restaurant> CreateReview(ReviewCreationDto dto);
+}
